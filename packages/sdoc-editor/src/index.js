4088b8ffd372a38a91015e19eae96a2ec2b701c2
shuntian <978987373@qq.com>
2025-05-28 11:45:25 +0800

    refactor(sdoc-editor): optimize code

--- packages/sdoc-editor/src/index.js
+++ packages/sdoc-editor/src/index.js
@@ -1,17 +1,22 @@
+
 import FileLoading from './components/file-loading';
 import Loading from './components/loading';
 import toaster from './components/toast';
 import Tooltip from './components/tooltip';
-import { INTERNAL_EVENT, FULL_WIDTH_MODE, PLUGIN_BTN_POSITION } from './constants';
+import { INTERNAL_EVENT, FULL_WIDTH_MODE, PLUGIN_BTN_POSITION, WIKI_EDITOR, WIKI_EDITOR_EDIT_AREA_WIDTH } from './constants';
 import context from './context';
 import RevisionEditor from './editor/revision-editor';
 import SDocEditor from './editor/sdoc-editor';
+import WikiEditor from './editor/wiki-editor';
+import { createWikiEditor } from './extension';
 import DropdownMenuItem from './extension/commons/dropdown-menu-item';
 import MenuShortcutPrompt from './extension/commons/menu-shortcut-indicator';
 import { CollaboratorsProvider, useCollaborators } from './hooks/use-collaborators';
 import { PluginsProvider, usePlugins } from './hooks/use-plugins';
+import withNodeId from './node-id';
 import SDocOutline from './outline';
 import { mdStringToSlate, slateToMdString, deserializeHtml, processor } from './slate-convert';
+import { withSocketIO } from './socket';
 import { isMac, isMobile } from './utils/common-utils';
 import { getTopLevelChanges, getMergedChanges } from './utils/diff';
 import EventBus from './utils/event-bus';
@@ -55,5 +60,10 @@ export {
   FULL_WIDTH_MODE,
   PLUGIN_BTN_POSITION,
   MenuShortcutPrompt,
-  DropdownMenuItem
+  DropdownMenuItem,
+  WIKI_EDITOR, WIKI_EDITOR_EDIT_AREA_WIDTH,
+  WikiEditor,
+  createWikiEditor,
+  withNodeId,
+  withSocketIO,
 };
