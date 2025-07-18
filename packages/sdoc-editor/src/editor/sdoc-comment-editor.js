09bc281274a90e6694c117f2fe8f456c94aa7243
Guodong Su <stephen.su66@gmail.com>
2025-07-18 17:30:02 +0800

    feat: mark comment as read

--- packages/sdoc-editor/src/editor/sdoc-comment-editor.js
+++ packages/sdoc-editor/src/editor/sdoc-comment-editor.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState, useImperativeHandle, forwardRef, useMemo, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Editor } from '@seafile/slate';
 import deepCopy from 'deep-copy';
 import PropTypes from 'prop-types';
+import { useNotificationContext } from '../comment/hooks/notification-hooks';
 import FileLoading from '../components/file-loading';
-import { COMMENT_EDITOR_EDIT_AREA_WIDTH } from '../constants';
+import toaster from '../components/toast';
+import { COMMENT_EDITOR_EDIT_AREA_WIDTH, INTERNAL_EVENT } from '../constants';
 import context from '../context';
 import { createCommentEditor } from '../extension';
 import { focusEditor } from '../extension/core';
@@ -13,12 +16,17 @@ import { ScrollContext } from '../hooks/use-scroll-context';
 import { EditorContainer } from '../layout';
 import withNodeId from '../node-id';
 import { withSocketIO } from '../socket';
+import { getErrorMsg } from '../utils/common-utils';
+import EventBus from '../utils/event-bus';
 import CommentArticle from './comment-article';
 
 const SdocCommentEditor = forwardRef(({ editor: propsEditor, document, isReloading, type, onSubmit, submitBtnText, onCancel }, ref) => {
 
   const [slateValue, setSlateValue] = useState(document.elements);
   const commentEditorContainerRef = useRef(null);
+  const commentTimeoutRef = useRef(null);
+  const { notificationsInfo } = useNotificationContext();
+  const { t } = useTranslation('sdoc-editor');
 
   const validEditor = useMemo(() => {
     if (propsEditor) return propsEditor;
@@ -33,6 +41,54 @@ const SdocCommentEditor = forwardRef(({ editor: propsEditor, document, isReloadi
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (!commentEditorContainerRef?.current) return;
+    if (Object.keys(notificationsInfo.notifications_map || {}).length === 0 ) return;
+    if (!commentEditorContainerRef.current.closest('.global-comment-input-wrapper')) return;
+
+    const clearAllNotification = async () => {
+      try {
+        await context.readAllNotifications();
+        const res = await context.listUnseenNotifications();
+        const notifications = res.data.notifications;
+        const eventBus = EventBus.getInstance();
+        eventBus.dispatch(INTERNAL_EVENT.UNSEEN_NOTIFICATIONS_COUNT, notifications?.length);
+        eventBus.dispatch(INTERNAL_EVENT.NEW_NOTIFICATION);
+        eventBus.dispatch(INTERNAL_EVENT.CLEAR_NOTIFICATION);
+      } catch (error) {
+        const errorMessage = getErrorMsg(error);
+        toaster.danger(t(errorMessage));
+      }
+    };
+
+    const el = commentEditorContainerRef && commentEditorContainerRef.current;
+
+    // Mark comment as read if focusing commentEditorContainerRef over 3s
+    const handleFocusIn = (e) => {
+      commentTimeoutRef.current = setTimeout(() => {
+        clearAllNotification();
+        commentTimeoutRef.current = null;
+      }, 3000);
+    };
+
+    const handleBlur = () => {
+      clearTimeout(commentTimeoutRef.current);
+      commentTimeoutRef.current = null;
+    };
+
+    el.addEventListener('focusin', handleFocusIn);
+    el.addEventListener('focusout', handleBlur);
+
+    return () => {
+      el.removeEventListener('focusin', handleFocusIn);
+      el.removeEventListener('focusout', handleBlur);
+
+      if (commentTimeoutRef.current) {
+        clearTimeout(commentTimeoutRef.current);
+        commentTimeoutRef.current = null;
+      }
+    };
+  }, [notificationsInfo]);
 
   // useMount: focus editor
   useEffect(() => {
