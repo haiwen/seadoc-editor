1e0d53b6543ad866092cfcdd749a69c410202bf9
Guodong Su <stephen.su66@gmail.com>
2025-07-31 14:36:18 +0800

    feat: add iframe

--- packages/sdoc-editor/src/extension/plugins/whiteboard/render-elem.js
+++ packages/sdoc-editor/src/extension/plugins/whiteboard/render-elem.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelected } from '@seafile/slate-react';
 import classNames from 'classnames';
+import { INTERNAL_EVENT } from '../../../constants';
 import context from '../../../context';
+import EventBus from '../../../utils/event-bus';
 
 import './index.css';
 
@@ -9,6 +11,35 @@ const Whiteboard = ({ editor, element }) => {
   const { filePath, repoID, title, link } = element;
   const whiteboardRef = useRef();
   const isSelected = useSelected();
+
+  useEffect(() => {
+
+    const handleMessage = (event) => {
+      if (event.data?.type === 'checkSdocParent') {
+        const isSdocClass = whiteboardRef?.current.classList.contains('sdoc-whiteboard-element');
+        whiteboardRef?.current.contentWindow.postMessage(
+          { type: 'checkSdocParentResult', isInSdoc: isSdocClass },
+          '*'
+        );
+      }
+    };
+
+    const handleWindowResize = () => {
+      whiteboardRef?.current.contentWindow.postMessage(
+        { type: 'resizeWindowWidth', isResize: true },
+        '*'
+      );
+    };
+
+
+    const eventBus = EventBus.getInstance();
+    const unsubscribeResizeArticle = eventBus.subscribe(INTERNAL_EVENT.RESIZE_ARTICLE, handleWindowResize);
+    window.addEventListener('message', handleMessage);
+    return () => {
+      window.removeEventListener('message', handleMessage);
+      unsubscribeResizeArticle();
+    };
+  }, []);
 
   const handleDoubleClick = (event) => {
     event.preventDefault();
@@ -19,15 +50,16 @@ const Whiteboard = ({ editor, element }) => {
   };
 
   return (
-    <div className={classNames('sdoc-whiteboard-container', { 'isSelected': isSelected })} ref={whiteboardRef} onDoubleClick={handleDoubleClick} scrolling='no'>
+    <div className={classNames('sdoc-whiteboard-container', { 'isSelected': isSelected })} onDoubleClick={handleDoubleClick} scrolling='no' >
       <div className='sdoc-whiteboard-title'>{title}</div>
       <iframe
         className='sdoc-whiteboard-element'
         title={title}
         src={link}
-        style={{ width: '100%', height: '100%', border: 'none', pointerEvents: 'none', }}
+        ref={whiteboardRef}
       >
       </iframe>
+      <div className='iframe-overlay' onDoubleClick={handleDoubleClick}></div>
     </div>
   );
 };
