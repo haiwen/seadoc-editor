4f70d0a8f1d1f36238a2f3ec30644a545ccf0b53
shuntian <978987373@qq.com>
2025-09-05 16:06:29 +0800

    fix: fix timeout bug

--- packages/seafile-sdoc-editor/src/components/tip-message/index.js
+++ packages/seafile-sdoc-editor/src/components/tip-message/index.js
@@ -27,6 +27,7 @@ class TipMessage extends React.Component {
     const eventBus = EventBus.getInstance();
     this.unsubscribeSavingEvent = eventBus.subscribe('is-saving', this.onDocumentSaving);
     this.unsubscribeSavedEvent = eventBus.subscribe('saved', this.onDocumentSaved);
+    this.unsubscribeSavedEvent = eventBus.subscribe('connect', this.onReconnect);
     // offline reconnect
     this.unsubscribeDisconnectEvent = eventBus.subscribe('disconnect', this.onDisconnect);
     this.unsubscribeReconnectErrorEvent = eventBus.subscribe('reconnect_error', this.onReconnectError);
@@ -150,9 +151,6 @@ class TipMessage extends React.Component {
       return <span className="tip-message">{t('Saving')}</span>;
     }
 
-    if (!isSaving && isSaved) {
-      return <span className="tip-message">{t('All_changes_saved')}</span>;
-    }
     if (lastSavedAt) {
       return (
         <span className='tip-message'>
