25020c72a191286506b1cf4e2206f1d4ad2ae951
Guodong Su <stephen.su66@gmail.com>
2025-12-08 10:17:23 +0800

    fix: update code

--- packages/sdoc-editor/src/cursor/use-cursors.js
+++ packages/sdoc-editor/src/cursor/use-cursors.js
@@ -1,20 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Editor, Point } from '@seafile/slate';
 
 export const useCursors = (editor) => {
   const [cursors, setCursors] = useState([]);
 
   useEffect(() => {
     const cursors = Object.values(editor.cursors) || [];
-    // Initially collapse the selections from all collaborators to  start point
-    const newCursors = cursors.map(cursor => {
-      if (!Point.equals(cursor.anchor, cursor.focus)) {
-        const frontPoint = Editor.start(editor, { anchor: cursor.anchor, focus: cursor.focus });
-        return { ...cursor, anchor: frontPoint, focus: frontPoint };
-      }
-      return cursor;
-    });
-    setCursors(newCursors);
+    setCursors(cursors);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
