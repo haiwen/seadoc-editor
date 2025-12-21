0c4e1ad535ffbbf1929ebb8e7be5c99cc80be07c
Guodong Su <stephen.su66@gmail.com>
2025-12-21 15:04:18 +0800

    feat: add mark for list

--- packages/sdoc-editor/src/extension/plugins/list/render-elem.js
+++ packages/sdoc-editor/src/extension/plugins/list/render-elem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Transforms } from '@seafile/slate';
+import { ReactEditor } from '@seafile/slate-react';
 import classnames from 'classnames';
 import { ORDERED_LIST } from '../../constants';
 
@@ -10,6 +12,7 @@ const renderList = (props, editor) => {
 
 const renderListItem = (props, editor) => {
   const { attributes, children, element } = props;
+  const collapsed = !!element.collapsed;
   const { align } = element.children[0];
   let className = '';
   switch (align) {
@@ -23,7 +26,34 @@ const renderListItem = (props, editor) => {
       className = '';
   }
   const isBold = element.children[0].children.every((item) => item.bold === true);
-  return <li data-id={element.id} {...attributes} className={classnames(className, { 'sdoc-li-bold': isBold })}>{children}</li>;
+
+  const togglePrefix = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const path = ReactEditor.findPath(editor, element);
+    Transforms.setNodes(
+      editor,
+      { collapsed: !collapsed },
+      { at: path }
+    );
+  };
+
+  const newChildren = !collapsed ? children : children[0];
+
+  return (
+    <li data-id={element.id} {...attributes} className={classnames(className, { 'sdoc-li-bold': isBold })}>
+      {element.children.length > 1 && (
+        <span className='sdoc-li-control' contentEditable='false'>
+          <span className={classnames('sdoc-li-prefix', 'sdocfont', (!collapsed ? 'arrow-down' : 'sdoc-next-page'))} onMouseDown={togglePrefix}></span>
+          <span className='sdoc-li-divider'></span>
+        </span>
+      )}
+      <span className='sdoc-li-content'>
+        {newChildren}
+      </span>
+    </li>
+  );
 };
 
 const renderListLic = (props, editor) => {
