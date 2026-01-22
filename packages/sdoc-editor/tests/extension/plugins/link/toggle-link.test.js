f5f89cd04cb72ff16179d5c5723b04f6d10e4e45
Guodong Su <stephen.su66@gmail.com>
2026-01-22 16:46:34 +0800

    fix: optimize test

--- packages/sdoc-editor/tests/extension/plugins/link/toggle-link.test.js
+++ packages/sdoc-editor/tests/extension/plugins/link/toggle-link.test.js
@@ -212,7 +212,7 @@ describe('modify link test', () => {
             测试链接
           </ha>
           <htext> </htext>
-          <ha href='http://localhost:7003/simple' title='测试' linked_id='' linked_wiki_page_id='' >
+          <ha href='http://localhost:7003/simple' title='测试' linked_id=''>
             测试
           </ha>
           <htext></htext>
