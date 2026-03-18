c7a7bb0a693e655240c4a677f708c2efa1b277fd
杨顺强 <978987373@qq.com>
2026-03-18 11:41:06 +0800

    fix: fix paste list html into sdoc bug (#207)

--- packages/sdoc-editor/src/slate-convert/html-to-slate/rules/list.js
+++ packages/sdoc-editor/src/slate-convert/html-to-slate/rules/list.js
@@ -1,25 +1,83 @@
 import slugid from 'slugid';
-import { LIST_ITEM, ORDERED_LIST, PARAGRAPH, UNORDERED_LIST } from '../constants';
+import { INLINE_LEVEL_TYPES, LIST_ITEM, ORDERED_LIST, PARAGRAPH, UNORDERED_LIST } from '../constants';
 
 const PARAGRAPH_TAGS = ['DIV', 'P'];
+const LIST_TYPES = [ORDERED_LIST, UNORDERED_LIST];
+
+const createParagraphNode = (children = []) => {
+  return {
+    id: slugid.nice(),
+    type: PARAGRAPH,
+    children
+  };
+};
+
+const normalizeListItemChildren = (nodes = []) => {
+  const nextChildren = [];
+  let paragraphChildren = [];
+
+  nodes.forEach(node => {
+    if (node.type === PARAGRAPH) {
+      if (paragraphChildren.length > 0) {
+        nextChildren.push(createParagraphNode(paragraphChildren));
+        paragraphChildren = [];
+      }
+      nextChildren.push(node);
+      return;
+    }
+
+    if (node.type && !INLINE_LEVEL_TYPES.includes(node.type) && !LIST_TYPES.includes(node.type)) {
+      if (paragraphChildren.length > 0) {
+        nextChildren.push(createParagraphNode(paragraphChildren));
+        paragraphChildren = [];
+      }
+      nextChildren.push(node);
+      return;
+    }
+
+    if (LIST_TYPES.includes(node.type)) {
+      if (paragraphChildren.length > 0) {
+        nextChildren.push(createParagraphNode(paragraphChildren));
+        paragraphChildren = [];
+      }
+      nextChildren.push(node);
+      return;
+    }
+
+    paragraphChildren.push(node);
+  });
+
+  if (paragraphChildren.length > 0) {
+    nextChildren.push(createParagraphNode(paragraphChildren));
+  }
+
+  if (nextChildren.length === 0) {
+    nextChildren.push(createParagraphNode([{ id: slugid.nice(), text: '' }]));
+  }
+
+  return nextChildren;
+};
 
 const listRule = (element, parseChild) => {
   const { nodeName, childNodes } = element;
   if (nodeName === 'UL') {
+    const validChildNodes = Array.from(childNodes).filter(item => item.nodeName === 'LI');
     return {
       id: slugid.nice(),
       type: UNORDERED_LIST,
-      children: parseChild(childNodes)
+      children: parseChild(validChildNodes)
     };
   }
   if (nodeName === 'OL') {
+    const validChildNodes = Array.from(childNodes).filter(item => item.nodeName === 'LI');
     return {
       id: slugid.nice(),
       type: ORDERED_LIST,
-      children: parseChild(childNodes)
+      children: parseChild(validChildNodes)
     };
   }
-  if (nodeName === 'LI' && PARAGRAPH_TAGS.includes(element.firstChild.nodeName)) {
+
+  if (nodeName === 'LI' && element.firstChild && PARAGRAPH_TAGS.includes(element.firstChild.nodeName)) {
     return {
       id: slugid.nice(),
       type: LIST_ITEM,
@@ -27,15 +85,12 @@ const listRule = (element, parseChild) => {
     };
   }
 
-  if (nodeName === 'LI' && !PARAGRAPH_TAGS.includes(element.firstChild.nodeName)) {
+  if (nodeName === 'LI') {
+    const parsedChildren = parseChild(childNodes);
     return {
       id: slugid.nice(),
       type: LIST_ITEM,
-      children: [{
-        id: slugid.nice(),
-        type: PARAGRAPH,
-        children: parseChild(childNodes)
-      }]
+      children: normalizeListItemChildren(parsedChildren)
     };
   }
 
