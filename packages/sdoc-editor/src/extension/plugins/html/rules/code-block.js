3a925a4ecfe66d4aed3929e3749f9658b0cdca6b
杨顺强 <978987373@qq.com>
2026-05-25 11:30:05 +0800

    fix: fix copy pycharm code bug (#247)

--- packages/sdoc-editor/src/extension/plugins/html/rules/code-block.js
+++ packages/sdoc-editor/src/extension/plugins/html/rules/code-block.js
@@ -2,40 +2,110 @@ import slugid from 'slugid';
 import { CODE_BLOCK, CODE_LINE } from '../../../constants';
 import { genCodeLangs } from '../../code-block/prismjs';
 
+const BLOCK_LINE_TAGS = ['DIV', 'P'];
+
+const normalizeCodeText = (text = '') => {
+  return text.replace(/\u00a0/g, ' ');
+};
+
+const isFormattingWhitespace = (text = '') => {
+  return text !== '' && text.trim() === '';
+};
+
+const appendTextToLines = (lines, text = '') => {
+  const segments = normalizeCodeText(text).replace(/\r\n?/g, '\n').split('\n');
+  lines[lines.length - 1] += segments[0] || '';
+  if (segments.length > 1) {
+    lines.push(...segments.slice(1));
+  }
+};
+
+const extractCodeLines = (nodes) => {
+  let lines = [''];
+
+  Array.from(nodes).forEach(node => {
+    if (node.nodeName === 'BR') {
+      lines.push('');
+      return;
+    }
+
+    if (node.nodeType === 3) {
+      const text = node.textContent || '';
+      if (isFormattingWhitespace(text)) return;
+      appendTextToLines(lines, text);
+      return;
+    }
+
+    if (BLOCK_LINE_TAGS.includes(node.nodeName)) {
+      const blockLines = extractCodeLines(node.childNodes);
+      if (blockLines.length === 0) {
+        lines.push('');
+        return;
+      }
+
+      if (lines.length === 1 && lines[0] === '') {
+        lines = blockLines;
+      } else {
+        lines[lines.length - 1] += blockLines[0] || '';
+        if (blockLines.length > 1) {
+          lines.push(...blockLines.slice(1));
+        }
+      }
+
+      const nextNode = node.nextSibling;
+      const hasNextBlockSibling = nextNode && BLOCK_LINE_TAGS.includes(nextNode.nodeName);
+      if (hasNextBlockSibling && blockLines[blockLines.length - 1] !== '') {
+        lines.push('');
+      }
+      return;
+    }
+
+    const nestedLines = extractCodeLines(node.childNodes);
+    if (nestedLines.length === 0) {
+      return;
+    }
+
+    if (lines.length === 1 && lines[0] === '') {
+      lines = nestedLines;
+      return;
+    }
+
+    lines[lines.length - 1] += nestedLines[0] || '';
+    if (nestedLines.length > 1) {
+      lines.push(...nestedLines.slice(1));
+    }
+  });
+
+  return lines;
+};
+
+const buildCodeLines = (element) => {
+  const lines = extractCodeLines(element.childNodes);
+
+  return lines.map(text => {
+    return {
+      id: slugid.nice(),
+      type: CODE_LINE,
+      children: [{
+        id: slugid.nice(),
+        text: text,
+      }],
+    };
+  });
+};
+
 const codeBlockRule = (element, parseChild) => {
   const { nodeName, childNodes } = element;
   if (nodeName === 'PRE') {
-    const children = Array.from(childNodes).filter(item => item.nodeName === 'CODE');
-    let codeChild = children[0];
-    if (codeChild) {
-      let lang = codeChild.getAttribute('lang');
-      lang = genCodeLangs().find(item => item.value === lang) || 'plaintext';
-      return {
-        id: slugid.nice(),
-        language: lang,
-        type: CODE_BLOCK,
-        children: parseChild(children)
-      };
-    } else {
-      const lang = 'plaintext';
-      const content = childNodes[0].textContent;
-      const children = content.split('\n').map(text => {
-        return {
-          id: slugid.nice(),
-          type: CODE_LINE,
-          children: [{
-            id: slugid.nice(),
-            text: text
-          }]
-        };
-      });
-      return {
-        id: slugid.nice(),
-        language: lang,
-        type: CODE_BLOCK,
-        children: children
-      };
-    }
+    const codeChild = element.querySelector('code');
+    let lang = codeChild?.getAttribute('lang');
+    lang = genCodeLangs().find(item => item.value === lang) || 'plaintext';
+    return {
+      id: slugid.nice(),
+      language: lang,
+      type: CODE_BLOCK,
+      children: buildCodeLines(codeChild || element)
+    };
   }
 
   if (nodeName === 'CODE' && element.parentElement.nodeName === 'PRE') {
--- packages/sdoc-editor/tests/extension/plugins/html/code-block.test.js
+++ packages/sdoc-editor/tests/extension/plugins/html/code-block.test.js
@@ -29,4 +29,184 @@ describe('deserialize code-block', () => {
     ];
     expect(formatChildren(ret)).toEqual(exp);
   });
+
+  it('pre with div lines to slate node', () => {
+    const html = [
+      '<pre>',
+      '<div>const seafile = \'seafile\';</div>',
+      '<div>const sdoc = \'editor\';</div>',
+      '</pre>'
+    ].join('');
+
+    const ret = deserializeHtml(html);
+    const exp = [
+      {
+        type: 'code_block',
+        language: 'plaintext',
+        children: [
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const seafile = \'seafile\';'
+              }
+            ]
+          },
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const sdoc = \'editor\';'
+              }
+            ]
+          }
+        ]
+      }
+    ];
+    expect(formatChildren(ret)).toEqual(exp);
+  });
+
+  it('pre with br lines to slate node', () => {
+    const html = [
+      '<pre>',
+      'const seafile = \'seafile\';<br>',
+      'const sdoc = \'editor\';',
+      '</pre>'
+    ].join('');
+
+    const ret = deserializeHtml(html);
+    const exp = [
+      {
+        type: 'code_block',
+        language: 'plaintext',
+        children: [
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const seafile = \'seafile\';'
+              }
+            ]
+          },
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const sdoc = \'editor\';'
+              }
+            ]
+          }
+        ]
+      }
+    ];
+    expect(formatChildren(ret)).toEqual(exp);
+  });
+
+  it('pre > code with div lines to slate node', () => {
+    const html = [
+      '<pre>',
+      '<code><div>const seafile = \'seafile\';</div><div>const sdoc = \'editor\';</div></code>',
+      '</pre>'
+    ].join('');
+
+    const ret = deserializeHtml(html);
+    const exp = [
+      {
+        type: 'code_block',
+        language: 'plaintext',
+        children: [
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const seafile = \'seafile\';'
+              }
+            ]
+          },
+          {
+            type: 'code_line',
+            children: [
+              {
+                text: 'const sdoc = \'editor\';'
+              }
+            ]
+          }
+        ]
+      }
+    ];
+    expect(formatChildren(ret)).toEqual(exp);
+  });
+
+  it('pycharm pre with span and br keeps code lines and blank lines', () => {
+    const html = [
+      '<html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"></head><body>',
+      '<div style="background-color:#191a1c;color:#bcbec4"><pre style="font-family:\'JetBrains Mono\',monospace;font-size:9.8pt;">',
+      '<span style="color:#c77dbb;">GITHUB_ISSUE_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'github_issue_added\'<br></span>',
+      '<span style="color:#c77dbb;">GITHUB_ISSUE_UPDATED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'github_issue_updated\'<br></span>',
+      '<span style="color:#c77dbb;">GITHUB_ISSUE_CLOSED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'github_issue_closed\'<br></span>',
+      '<span style="color:#c77dbb;">GITHUB_ISSUE_REOPENED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'github_issue_reopened\'<br></span>',
+      '<span style="color:#c77dbb;">GITHUB_ISSUE_COMMENT_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'github_issue_comment_added\'<br></span>',
+      '<span style="color:#c77dbb;">DISCOURSE_TOPIC_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'discourse_topic_added\'<br></span>',
+      '<span style="color:#c77dbb;">DISCOURSE_TOPIC_UPDATED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'discourse_topic_updated\'<br></span>',
+      '<span style="color:#c77dbb;">DISCOURSE_TOPIC_REPLY_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'discourse_topic_reply_added\'<br></span>',
+      '<span style="color:#c77dbb;">EMAIL_THREAD_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'email_thread_added\'<br></span>',
+      '<span style="color:#c77dbb;">EMAIL_MESSAGE_ADDED&#32;</span>=&#32;',
+      '<span style="color:#6aab73;">\'email_message_added\'</span>',
+      '</pre></div></body></html>'
+    ].join('');
+
+    const ret = deserializeHtml(html);
+    const exp = [
+      {
+        type: 'code_block',
+        language: 'plaintext',
+        children: [
+          { type: 'code_line', children: [{ text: 'GITHUB_ISSUE_ADDED = \'github_issue_added\'' }] },
+          { type: 'code_line', children: [{ text: 'GITHUB_ISSUE_UPDATED = \'github_issue_updated\'' }] },
+          { type: 'code_line', children: [{ text: 'GITHUB_ISSUE_CLOSED = \'github_issue_closed\'' }] },
+          { type: 'code_line', children: [{ text: 'GITHUB_ISSUE_REOPENED = \'github_issue_reopened\'' }] },
+          { type: 'code_line', children: [{ text: 'GITHUB_ISSUE_COMMENT_ADDED = \'github_issue_comment_added\'' }] },
+          { type: 'code_line', children: [{ text: 'DISCOURSE_TOPIC_ADDED = \'discourse_topic_added\'' }] },
+          { type: 'code_line', children: [{ text: 'DISCOURSE_TOPIC_UPDATED = \'discourse_topic_updated\'' }] },
+          { type: 'code_line', children: [{ text: 'DISCOURSE_TOPIC_REPLY_ADDED = \'discourse_topic_reply_added\'' }] },
+          { type: 'code_line', children: [{ text: 'EMAIL_THREAD_ADDED = \'email_thread_added\'' }] },
+          { type: 'code_line', children: [{ text: 'EMAIL_MESSAGE_ADDED = \'email_message_added\'' }] },
+        ]
+      }
+    ];
+    expect(formatChildren(ret)).toEqual(exp);
+  });
+
+  it('pre with div wrappers preserves blank lines', () => {
+    const html = [
+      '<pre>',
+      '<div>const seafile = \'seafile\';</div>',
+      '<div><br></div>',
+      '<div>const sdoc = \'editor\';</div>',
+      '</pre>'
+    ].join('');
+
+    const ret = deserializeHtml(html);
+    const exp = [
+      {
+        type: 'code_block',
+        language: 'plaintext',
+        children: [
+          { type: 'code_line', children: [{ text: 'const seafile = \'seafile\';' }] },
+          { type: 'code_line', children: [{ text: '' }] },
+          { type: 'code_line', children: [{ text: 'const sdoc = \'editor\';' }] },
+        ]
+      }
+    ];
+    expect(formatChildren(ret)).toEqual(exp);
+  });
 });
